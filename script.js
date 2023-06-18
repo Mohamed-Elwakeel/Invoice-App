ef97229d03c6906ef61395a71ef5e56f47433b5b
Mohamed Elwakeel <mo7mdhassan7@gmail.com>
2023-06-18 20:48:01 +0300

    made the base script file & connected to mongoDB

---
+++ script.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const fs = require("fs");
+
+const { db } = require("./db/db");
+const routePath = "/api/v1";
+
+require("dotenv").config();
+
+const app = express();
+
+// middleware
+app.use(express.json());
+app.use(cors({}));
+
+// app.get("/", (req, res) => {
+//   res.send("Hello World!!");
+// });
+
+//routes
+fs.readdir("./routes", (err, files) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  files.forEach((file) => {
+    const routeHandler = require("./routes/" + file);
+    app.use(routePath, routeHandler);
+  });
+});
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/invoice-DB")
+  .then(() => {
+    console.log("DB Connected");
+  })
+  .catch((err) => console.log("DB Connection Error: ", err));
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is listening on port ${process.env.PORT}`);
+});
